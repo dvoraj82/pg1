e8d3a46d452d5341641e3a6bb41ef091877a5d9d
dvoraj82 <dvoraj82@jcu.cz>
2025-10-02 08:51:05 +0000

    první cvičení

--- cviceni1.py
+++ cviceni1.py
@@ -1,1 +1,33 @@
 
+
+#def main():
+    #vstup = int(input("Zadej číslo: "))
+
+    #if vstup > 10:
+    #    print(f"Číslo {vstup} je větší než 10")
+    #elif vstup < 10:
+    #    print(f"Číslo {vstup} je menší než 10")
+    #else:
+    #    print(f"Vaše číslo {vstup} je rovno 10")
+
+    #otevreny_soubor = open("soubor.txt", "r")
+    #obsah_souboru = otevreny_soubor.read()
+    #print(obsah_souboru)
+
+
+#if __name__ == "__main__":
+#    main()
+
+
+#netusšim co to je
+#def delitelne_beze_zbytku(cislo):
+#    return False
+
+#if __name__ == "__main__":
+#    vstup = int(input("Zadej číslo: "))
+#    vstup % 3
+#
+#    if delitelne_beze_zbytku():
+#       print("Ano, je děl. beze zbytku")
+#   else:
+#       print("Ne, není děl. beze zbytku")
